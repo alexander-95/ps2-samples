ed85436d45b62a740b47caffbb2f6aac2b14b47d
Alexander Mitchell <alex.mitchell.1995@gmail.com>
2018-11-04 23:48:04 +0000

    fixed formatting

--- mc_example/icn2array.py
+++ mc_example/icn2array.py
@@ -8,7 +8,7 @@ print len(content)
 icn_size = len(content)/2
 
 # generate icon.c
-output = '#include \"icon.h\"\n'
+output = '#include \"icon.h\"\n\n'
 
 # generate byte array from icn hex-dump
 icn = 'extern const u8 icn_array[] = {'
@@ -30,11 +30,11 @@ sys = 'extern const u8 sys_array[] = {'
 for i in range(0, len(content),2):
     sys += '0x'+content[i:i+2].upper()+', '
 sys = sys[:-2]
-sys += "};"
+sys += "};\n\n"
 output += sys
 
 output += 'extern const u32 icn_size = '+str(icn_size)+';\n'
-output += 'extern const u32 sys_size = '+str(sys_size)+';\n\n'
+output += 'extern const u32 sys_size = '+str(sys_size)+';\n'
 
 c = open('icon.c', 'w')
 c.write(output)
