36b4079b6b56eb7715dea925d38a5084e2079c9e
Alexander Mitchell <alex.mitchell.1995@gmail.com>
2018-12-09 00:36:17 +0000

    partially implemented a mario map

---
+++ tiled/tiled.cpp
@@ -0,0 +1,345 @@
+//  ____     ___ ____
+//  ____|   |    ____|
+// |     ___|   |____
+
+#include <gsKit.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <kernel.h>
+#include <sifrpc.h>
+#include <loadfile.h>
+#include <libpad.h>
+#include <dmaKit.h>
+#include <gsToolkit.h>
+#include <malloc.h>
+
+#include "spritesheet.h"
+
+void loadModules();
+int waitPadReady(int port, int slot);
+int initializePad(int port, int slot);
+void openPad(int port, int slot, int padBuf[256]);
+void stabilise(int port, int slot);
+
+void gsKit_texture_abgr(GSGLOBAL* gsGlobal, GSTEXTURE* tex, u32* arr, u32 width, u32 height)
+{
+    u32 VramTextureSize = gsKit_texture_size(width, height, GS_PSM_CT32);
+
+    tex->Width = width;
+    tex->Height = height;
+    tex->PSM = GS_PSM_CT32;
+    tex->ClutPSM = 0;
+    tex->TBW = 1;
+    tex->Mem = arr;
+    tex->Clut = NULL;
+    tex->Vram = gsKit_vram_alloc(gsGlobal, VramTextureSize, GSKIT_ALLOC_USERBUFFER);
+    tex->VramClut = 0;
+    tex->Filter = GS_FILTER_NEAREST;
+    tex->Delayed = 0;
+
+    gsKit_texture_upload(gsGlobal, tex);
+}
+
+static int padBuf[256] __attribute__((aligned(64)));
+
+int main()
+{
+    // place the top corner of the screen at this location of the map and render the first tile.
+    int x = 0, y=0;
+    int tile_x, tile_y;
+    int index;
+    int point_x, point_y; 
+    int index_x, index_y; // index of current tile
+    int start_x, start_y; //top left corner of current tile
+    int scale_factor; 
+    u8 value; // value being read from map array
+    
+    int map_width = 224, map_height = 15; // array size
+    u8 map[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                00,00,00,00,00,00,00,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                00,00,13,00,00,00,00,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                00,10,11,12,00,00,00,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                10,11,11,11,12,00,00,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                01,01,01,01,01,01,01,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                01,01,01,01,01,01,01,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,};
+
+    GSGLOBAL* gsGlobal = gsKit_init_global();
+    gsGlobal->Mode = GS_MODE_PAL;
+    gsGlobal->Width=640;
+    gsGlobal->Height=512;
+    gsGlobal->PSMZ = GS_PSMZ_16S;
+    gsGlobal->ZBuffering = GS_SETTING_ON;
+    gsGlobal->PrimAlphaEnable = GS_SETTING_ON;
+    gsGlobal->DoubleBuffering = GS_SETTING_ON;
+
+        GSTEXTURE spriteSheet;
+    spriteSheet.Width = 128;
+    spriteSheet.Height = 64;
+    spriteSheet.PSM = GS_PSM_CT32;
+    int tile_width = 16, tile_height = 16;
+    
+
+    u64 White = GS_SETREG_RGBAQ(0xFF,0xFF,0xFF,0x00,0x00);// set color
+    dmaKit_init(D_CTRL_RELE_OFF, D_CTRL_MFD_OFF, D_CTRL_STS_UNSPEC,
+                D_CTRL_STD_OFF, D_CTRL_RCYC_8, 1 << DMA_CHANNEL_GIF);
+
+    //Initialize the DMAC
+    dmaKit_chan_init(DMA_CHANNEL_GIF);
+    gsKit_init_screen(gsGlobal);
+    gsKit_set_clamp(gsGlobal, GS_CMODE_CLAMP);
+    gsKit_mode_switch(gsGlobal, GS_ONESHOT);
+
+    
+    gsKit_texture_abgr(gsGlobal, &spriteSheet, spritesheet_array, spriteSheet.Width, spriteSheet.Height );
+
+    //controller setup
+    unsigned int old_pad = 0;
+    unsigned int new_pad, paddata;
+    int port=0, slot=0;
+    struct padButtonStatus buttons;
+    SifInitRpc(0);
+    loadModules();
+    padInit(0);
+    openPad(port,slot,padBuf);
+
+    
+    while(1)
+    {
+        if(padRead(port, slot, &buttons) != 0)
+        {
+            paddata = 0xffff ^ buttons.btns;
+            new_pad = paddata & ~old_pad;
+            old_pad = paddata;
+            if(old_pad & PAD_LEFT)x-=2;
+            else if(old_pad & PAD_RIGHT)x+=2;
+            else if(old_pad & PAD_UP)y-=2;
+            else if(old_pad & PAD_DOWN)y+=2;
+        }
+        
+        // figure out the tile to draw
+        tile_x = x / tile_width;
+        tile_y = y / tile_height;
+        //printf("(%d, %d)", tile_x, tile_y);
+
+        //printf("draw tile at location (%d, %d)\n", tile_x, tile_y);
+        index = tile_y * map_width + tile_x;
+        value = map[index];
+
+        // where do we start in the tile
+        point_x = x % tile_width;
+        point_y = y % tile_height;
+
+        // the spritesheet is actually 8x4 rather than 32x1
+        index_x = value % 8;
+        index_y = (value / 8);
+        //printf("value = %d\n", value);
+        
+        start_x = 0 - point_x, start_y = 0 - point_y;
+        scale_factor = 2;
+        while(start_y < 512 && tile_y < map_height)
+        {
+            while(start_x < 640 && tile_x < map_width){
+                index = tile_y * map_width + tile_x;
+                value = map[index];
+                index_x = value % 8;
+                index_y = value / 8;
+                //printf("drawing tile at(%d, %d)\n", start_x, start_y);
+                gsKit_prim_quad_texture(gsGlobal, &spriteSheet,
+                                        start_x*scale_factor, start_y*scale_factor,                                // x1, y1
+                                        tile_width*index_x, tile_height*index_y,         // u1, v1
+                                    
+                                        start_x*scale_factor, (start_y + tile_height)*scale_factor,                  // x2, y2
+                                        tile_width*index_x, tile_height*(index_y+1),     // u2, y2
+
+                                        (start_x + tile_width)*scale_factor, start_y*scale_factor,                   // x3, y3
+                                        tile_width*(index_x+1), tile_height*index_y,     // u3, v3
+                            
+                                        (start_x + tile_width)*scale_factor, (start_y + tile_height)*scale_factor,     // x4, y4
+                                        tile_width*(index_x+1), tile_height*(index_y+1), // u4, v4
+                                        0,White);
+                start_x += tile_width;
+                tile_x++;
+            }
+            start_x = 0 - point_x;
+            tile_x = x / tile_width;
+            start_y += tile_height;
+            tile_y++;
+                
+        }
+        gsKit_sync_flip(gsGlobal);
+        gsKit_queue_exec(gsGlobal);
+        gsKit_queue_reset(gsGlobal->Per_Queue);
+        gsKit_clear(gsGlobal, 0);
+    }
+    return 0;
+}
+
+void loadModules()
+{
+    int ret=0;
+    ret = SifLoadModule("rom:SIO2MAN", 0, NULL);
+    if (ret < 0)ret = SifLoadModule("host:SIO2MAN", 0, NULL);
+    if (ret < 0)
+    {
+        printf("sifLoadModule sio failed: %d\n", ret);
+        SleepThread();
+    }
+   
+    ret = SifLoadModule("rom:PADMAN", 0, NULL);
+    if(ret < 0)ret = SifLoadModule("host:PADMAN", 0, NULL);
+    if (ret < 0)
+    {
+        printf("sifLoadModule pad failed: %d\n", ret);
+        
+        SleepThread();
+    }
+    
+}
+
+void openPad(int port, int slot, int padBuf[256])
+{
+  int ret;
+  if((ret = padPortOpen(port, slot, padBuf)) == 0)
+    {
+        printf("padOpenPort failed: %d\n", ret);
+        SleepThread();
+    }
+    if(!initializePad(port, slot))
+    {
+        printf("pad initalization failed!\n");
+        SleepThread();
+    }
+}
+
+void stabilise(int port, int slot)
+{
+  int ret;
+  ret=padGetState(port, slot);
+  while((ret != PAD_STATE_STABLE) && (ret != PAD_STATE_FINDCTP1))
+  {
+      if(ret==PAD_STATE_DISCONN) printf("Pad(%d, %d) is disconnected\n", port, slot);
+      ret = padGetState(port, slot);
+  }
+  //printf("Pad: OK!\n");
+}
+
+int waitPadReady(int port, int slot)
+{
+    int state;
+    int lastState;
+    char stateString[16];
+    state = padGetState(port, slot);
+    lastState = -1;
+    while((state != PAD_STATE_STABLE) && (state != PAD_STATE_FINDCTP1))
+    {
+        if (state != lastState)
+        {
+            padStateInt2String(state, stateString);
+            printf("Please wait, pad(%d,%d) is in state %s\n",
+                   port, slot, stateString);
+        }
+        lastState = state;
+        state=padGetState(port, slot);
+        }
+    // Were the pad ever 'out of sync'?
+    if (lastState != -1)
+    {
+        printf("Pad OK!\n");
+        }
+    return 0;
+}
+
+int initializePad(int port, int slot)
+{
+    static int actuators;
+    static char actAlign[6];
+
+    int ret;
+    int modes;
+    int i;
+
+    waitPadReady(port, slot);
+
+    // How many different modes can this device operate in?
+    // i.e. get # entrys in the modetable
+    modes = padInfoMode(port, slot, PAD_MODETABLE, -1);
+    printf("The device has %d modes\n", modes);
+
+    if (modes > 0) {
+        printf("( ");
+        for (i = 0; i < modes; i++) {
+            printf("%d ", padInfoMode(port, slot, PAD_MODETABLE, i));
+        }
+        printf(")");
+    }
+
+    printf("It is currently using mode %d\n",
+           padInfoMode(port, slot, PAD_MODECURID, 0));
+
+    // If modes == 0, this is not a Dual shock controller
+    // (it has no actuator engines)
+    if (modes == 0) {
+        printf("This is a digital controller?\n");
+        return 1;
+    }
+
+    // Verify that the controller has a DUAL SHOCK mode
+    i = 0;
+    do {
+        if (padInfoMode(port, slot, PAD_MODETABLE, i) == PAD_TYPE_DUALSHOCK)
+            break;
+        i++;
+    } while (i < modes);
+    if (i >= modes) {
+        printf("This is no Dual Shock controller\n");
+        return 1;
+    }
+
+    // If ExId != 0x0 => This controller has actuator engines
+    // This check should always pass if the Dual Shock test above passed
+    ret = padInfoMode(port, slot, PAD_MODECUREXID, 0);
+    if (ret == 0) {
+        printf("This is no Dual Shock controller??\n");
+        return 1;
+    }
+
+    printf("Enabling dual shock functions\n");
+
+    // When using MMODE_LOCK, user cant change mode with Select button
+    padSetMainMode(port, slot, PAD_MMODE_DUALSHOCK, PAD_MMODE_LOCK);
+
+    waitPadReady(port, slot);
+    printf("infoPressMode: %d\n", padInfoPressMode(port, slot));
+
+    waitPadReady(port, slot);
+    printf("enterPressMode: %d\n", padEnterPressMode(port, slot));
+    
+    
+    waitPadReady(port, slot);
+    actuators = padInfoAct(port, slot, -1, 0);
+    printf("# of actuators: %d\n",actuators);
+
+    if (actuators != 0)
+    {
+        actAlign[0] = 0;   // Enable small engine
+        actAlign[1] = 1;   // Enable big engine
+        actAlign[2] = 0xff;
+        actAlign[3] = 0xff;
+        actAlign[4] = 0xff;
+        actAlign[5] = 0xff;
+        waitPadReady(port, slot);
+        printf("padSetActAlign: %d\n", padSetActAlign(port, slot, actAlign));
+    }
+    else printf("Did not find any actuators.\n");
+    waitPadReady(port, slot);
+    return 1;
+}
