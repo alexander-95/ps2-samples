b447ca85c3c23b80b5ca490dda5f017d7fcdc6b1
Alexander Mitchell <alex.mitchell.1995@gmail.com>
2019-10-08 01:10:42 +0100

    removing more redundant files

--- tiled/mario.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#ifndef MARIO_H
-#define MARIO_H
-
-extern u32 mario_array[];
-
-#endif
--- tiled/pickups.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#ifndef PICKUPS_H
-#define PICKUPS_H
-
-extern u32 pickups_array[];
-
-#endif
