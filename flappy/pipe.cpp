52b0ec465d10135e8cabac34f49bac35615a3a86
Alexander Mitchell <alex.mitchell.1995@gmail.com>
2019-10-22 22:26:55 +0100

    fixed pipe print function

--- flappy/pipe.cpp
+++ flappy/pipe.cpp
@@ -22,8 +22,8 @@ void Pipe::print()
     printf("y           = %d\n", y);
     printf("d           = %d\n", d);
     printf("nightMode   = %d\n", nightMode);
-    printf("next        = %x\n", next);
-    printf("prev        = %x\n", prev);
-    printf("gsGlobal    = %x\n", gsGlobal);
-    printf("spritesheet = %x\n\n", spritesheet);
+    printf("next        = %p\n", next);
+    printf("prev        = %p\n", prev);
+    printf("gsGlobal    = %p\n", gsGlobal);
+    printf("spritesheet = %p\n\n", spritesheet);
 }
